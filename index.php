7c710f632ac55e3108d56eabc53e1675bad62000
Seif Ayman <seif.tanjiro@gmail.com>
2025-01-30 15:52:06 +0200

    Add Cute Style

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php
+function generateLinks($links, $sectionTitle)
+{
+    echo '<div class="link-section">';
+    echo '<h3>' . $sectionTitle . '</h3>';
+    echo '<ul>';
+    foreach ($links as $link) {
+        echo '<li><a href="' . $link['url'] . '">' . $link['text'] . '</a></li>';
+    }
+    echo '</ul>';
+    echo '</div>';
+}
+
+$oldLinks = [
+    ['url' => '/layout/php/simple_greeting.php', 'text' => 'Simple Greeting'],
+    ['url' => '/layout/php/age_message.php', 'text' => 'Age Message'],
+    ['url' => '/layout/php/sum_of_two_numbers.php', 'text' => 'Sum of Two Numbers'],
+    ['url' => '/layout/php/multiplication_table.php', 'text' => 'Multiplication Table'],
+    ['url' => '/layout/php/full_name.php', 'text' => 'Full Name'],
+    ['url' => '/layout/php/person_introduction.php', 'text' => 'Person Introduction'],
+    ['url' => '/layout/php/simple_discount.php', 'text' => 'Simple Discount'],
+    ['url' => '/layout/php/rectangle_area.php', 'text' => 'Rectangle Area'],
+    ['url' => '/layout/php/simple_calculator.php', 'text' => 'Simple Calculator'],
+    ['url' => '/layout/php/temperature_converter.php', 'text' => 'Temperature Converter'],
+    ['url' => '/layout/php/circle_area.php', 'text' => 'Circle Area'],
+    ['url' => '/layout/php/discount_price.php', 'text' => 'Discount Price'],
+    ['url' => '/layout/php/concat_strings.php', 'text' => 'Concat Strings'],
+    ['url' => '/layout/php/exchange_rate.php', 'text' => 'Exchange Rate'],
+    ['url' => '/layout/php/add_prefix_to_name.php', 'text' => 'Add Prefix to Name'],
+    ['url' => '/layout/php/string_length.php', 'text' => 'Length of a String'],
+    ['url' => '/layout/php/personalized_welcome.php', 'text' => 'Personalized Welcome'],
+    ['url' => '/layout/php/power_of_number.php', 'text' => 'Power of Number'],
+];
+
+$dumbLinks=[
+    ['url' => '/layout/dump/dump.php', 'text' => 'Dumb'],
+    ['url' => '/layout/dump/global.php', 'text' => 'Global'],
+];
+
+$newLinks = [
+    ['url' => '/layout/function/addNumbers.php', 'text' => 'Add Numbers'],
+    ['url' => '/layout/function/multiply.php', 'text' => 'Multiply'],
+    ['url' => '/layout/function/divide.php', 'text' => 'Divide'],
+    ['url' => '/layout/function/calculateArea.php', 'text' => 'Calculate Area'],
+    ['url' => '/layout/function/square.php', 'text' => 'Square'],
+    ['url' => '/layout/function/cube.php', 'text' => 'Cube'],
+    ['url' => '/layout/function/convertToMinutes.php', 'text' => 'Convert Hours to Minutes'],
+    ['url' => '/layout/function/convertToSeconds.php', 'text' => 'Convert Days to Seconds'],
+    ['url' => '/layout/function/calculatePerimeter.php', 'text' => 'Calculate Perimeter'],
+    ['url' => '/layout/function/average.php', 'text' => 'Calculate Average'],
+    ['url' => '/layout/function/convertTemperature.php', 'text' => 'Convert Celsius to Fahrenheit'],
+    ['url' => '/layout/function/calculateSpeed.php', 'text' => 'Calculate Speed'],
+    ['url' => '/layout/function/hypotenuse.php', 'text' => 'Calculate Hypotenuse'],
+    ['url' => '/layout/function/power.php', 'text' => 'Calculate Power'],
+    ['url' => '/layout/function/modulus.php', 'text' => 'Calculate Modulus'],
+    ['url' => '/layout/function/calculateBMI.php', 'text' => 'Calculate BMI'],
+    ['url' => '/layout/function/convertToKilometers.php', 'text' => 'Convert Meters to Kilometers'],
+    ['url' => '/layout/function/calculateInterest.php', 'text' => 'Calculate Simple Interest'],
+    ['url' => '/layout/function/compoundInterest.php', 'text' => 'Calculate Compound Interest'],
+    ['url' => '/layout/function/findPercentage.php', 'text' => 'Calculate Percentage'],
+];
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +68,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Challenges</title>
     <link rel="stylesheet" href="/layout/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DynaPuff:wght@400..700&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -16,28 +81,14 @@
         <h2>Seif Ayman Ahmed</h2>
     </div>
 
-    <nav>
-        <ul>
-            <li><a href="/layout/php/simple_greeting.php">Simple Greeting</a></li>
-            <li><a href="/layout/php/age_message.php">Age Message</a></li>
-            <li><a href="/layout/php/sum_of_two_numbers.php">Sum of Two Numbers</a></li>
-            <li><a href="/layout/php/multiplication_table.php">Multiplication Table</a></li>
-            <li><a href="/layout/php/full_name.php">Full Name</a></li>
-            <li><a href="/layout/php/person_introduction.php">Person Introduction</a></li>
-            <li><a href="/layout/php/simple_discount.php">Simple Discount</a></li>
-            <li><a href="/layout/php/rectangle_area.php">Rectangle Area</a></li>
-            <li><a href="/layout/php/simple_calculator.php">Simple Calculator</a></li>
-            <li><a href="/layout/php/temperature_converter.php">Temperature Converter</a></li>
-            <li><a href="/layout/php/circle_area.php">Circle Area</a></li>
-            <li><a href="/layout/php/discount_price.php">Discount Price</a></li>
-            <li><a href="/layout/php/concat_strings.php">Concat Strings</a></li>
-            <li><a href="/layout/php/exchange_rate.php">Exchange Rate</a></li>
-            <li><a href="/layout/php/add_prefix_to_name.php">Add Prefix to Name</a></li>
-            <li><a href="/layout/php/string_length.php">Length of a String</a></li>
-            <li><a href="/layout/php/personalized_welcome.php">Personalized Welcome</a></li>
-            <li><a href="/layout/php/power_of_number.php">Power of Number</a></li>
-        </ul>
-    </nav>
+    <?php
+    // Old Links Section
+    generateLinks($oldLinks, 'Old Resources');
+    generateLinks($dumbLinks, 'Dumb Resources');
+    // New Links Section
+    generateLinks($newLinks, 'New Resources');
+    ?>
+
 </body>
 
 </html>
--- layout/dump/global.php
+++ layout/dump/global.php
@@ -1,53 +1,85 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$total = 0;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Output in Styled Terminal</title>
+    <link rel="stylesheet" href="/layout/css/terminal.css">
+</head>
 
-const VAT = 0.14;
-const SRV = 0.05;
-function add_sales($price)
-{
-    $vat = calc_vat($price);
-    $srv = calc_service($price);
+<body>
+    <div class="card">
+        <div class="wrap">
+            <div class="terminal">
+                <hgroup class="head">
+                    <p class="title">
+                        <svg width="16px" height="16px" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="currentColor" fill="none">
+                            <path d="M7 15L10 12L7 9M13 15H17M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z"></path>
+                        </svg>
+                        Terminal
+                    </p>
+                </hgroup>
+                <div class="body">
+                    <?php
 
-    global $total;
-    $total += $price + VAT;
-}
+                    $total = 0;
 
-function report()
-{
-    global $total;
-    echo "Total sales: $total<br>";
-}
+                    const VAT = 0.14;
+                    const SRV = 0.05;
+                    function add_sales($price)
+                    {
+                        $vat = calc_vat($price);
+                        $srv = calc_service($price);
 
-function refund($price)
-{
-    global $total;
+                        global $total;
+                        $total += $price + VAT;
+                    }
 
-    $total -= $price;
-}
+                    function report()
+                    {
+                        global $total;
+                        echo "Total sales: $total<br>";
+                    }
 
-function calc_vat($price)
-{
-    $total = $price + $price * VAT;
-    echo "Total vat: $total<br>";
-    return $price * VAT;
-}
+                    function refund($price)
+                    {
+                        global $total;
 
-function calc_service($price)
-{
-    global $total;
-    $total += $total * SRV;
-    // echo "Total service fee: $total<br>";
-    return $price * SRV;
-}
+                        $total -= $price;
+                    }
 
-report();
+                    function calc_vat($price)
+                    {
+                        $total = $price + $price * VAT;
+                        echo "Total vat: $total<br>";
+                        return $price * VAT;
+                    }
 
-add_sales(1000);
-report();
-add_sales(4500);
-add_sales(5000);
-add_sales(1500);
+                    function calc_service($price)
+                    {
+                        global $total;
+                        $total += $total * SRV;
+                        // echo "Total service fee: $total<br>";
+                        return $price * SRV;
+                    }
 
-refund(1000);
-report();
+                    report();
+
+                    add_sales(1000);
+                    report();
+                    add_sales(4500);
+                    add_sales(5000);
+                    add_sales(1500);
+
+                    refund(1000);
+                    report();
+                    ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
